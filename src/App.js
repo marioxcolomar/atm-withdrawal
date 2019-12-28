026616914c4d6be7ccf4b856f4ae84513acd916f
marioxcolomar <marioxcolomar@gmail.com>
2019-12-28 14:32:19 +0100

    Clean up App.js and add withdrawal (WIP).

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Withdrawal from './components/withdrawal';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Withdrawal />
+		</div>
+	);
 }
 
 export default App;
